063668a9fc29da5d7e1ab601478d65e42e4d4d90
Viola Harperath <viola.harperath@kcl.ac.uk>
2022-10-01 17:31:45 +0100

    First attempt at returning n number of primes

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
+    current_value = 2
+    while len(list) < number_of_primes:
+        for i in range(2, current_value+1):
+            if(current_value == i):
+                list.append(i)
+                current_value +=1
+            elif(current_value%i ==0):
+                current_value +=1
+                break
     return list
